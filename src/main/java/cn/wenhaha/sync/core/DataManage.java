fbf509583ad1ec395fad559f816919a2886e46c8
wyndem <wyndem@qq.copm>
2022-08-11 23:07:41 +0800

    初始化工程

---
+++ src/main/java/cn/wenhaha/sync/core/DataManage.java
@@ -0,0 +1,15 @@
+package cn.wenhaha.sync.core;
+
+/**
+ * 数据处理管理器
+ * --------
+ *
+ * @author ：wyndem
+ * @Date ：Created in 2022-08-11 21:13
+ */
+public interface DataManage {
+
+
+
+
+}
---
+++ src/main/java/cn/wenhaha/sync/core/DataSyncCoreContext.java
@@ -0,0 +1,27 @@
+package cn.wenhaha.sync.core;
+
+/**
+ *
+ * 同步核心上下文
+ * --------
+ *
+ * @author ：wyndem
+ * @Date ：Created in 2022-08-11 21:12
+ */
+public interface DataSyncCoreContext {
+
+    /**
+     *  查询：
+     *      1. 待同步的数据
+     *      2. 需要定制的关联关系
+     */
+
+
+
+    //更新
+
+
+
+
+
+}
